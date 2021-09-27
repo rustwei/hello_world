4af78226bc9440ac3778ece2fc2e58f91f62d542
魏山巍 <shanwei.wei@bl.com>
2021-09-27 13:16:48 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,205 @@
+mod myfile;
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Draw Button")
+    }
+}
+
+// pub trait Messenger {
+//     fn send(&self, msg: &str);
+// }
+//
+// pub struct LimitTracker<'a, T: Messenger> {
+//     messenger: &'a T,
+//     value: usize,
+//     max: usize,
+// }
+//
+// impl<'a, T> LimitTracker<'a, T>
+//     where T: Messenger {
+//     pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+//         LimitTracker {
+//             messenger,
+//             value: 0,
+//             max,
+//         }
+//     }
+//
+//     pub fn set_value(&mut self, value: usize) {
+//         self.value = value;
+//         let percentage_of_max = self.value as f64 / self.max as f64;
+//
+//         if percentage_of_max >= 1.0 {
+//             self.messenger.send("Error: You are over your quota!");
+//         } else if percentage_of_max >= 0.9 {
+//             self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+//         } else if percentage_of_max >= 0.75 {
+//             self.messenger.send("Warning: You've used up over 75% of your quota!");
+//         }
+//     }
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use std::cell::RefCell;
+//
+//     struct MockMessenger {
+//         sent_messages: RefCell<Vec<String>>
+//     }
+//
+//     impl MockMessenger {
+//         fn new() -> MockMessenger {
+//             MockMessenger {
+//                 sent_messages: RefCell::new(vec![])
+//             }
+//         }
+//     }
+//
+//     impl Messenger for MockMessenger {
+//         fn send(&self, message: &str) {
+//             self.sent_messages.borrow_mut().push(String::from(message))
+//         }
+//     }
+//
+//     #[test]
+//     fn it_sends_an_over_75_percent_warning_message() {
+//         let mock_messeger = MockMessenger::new();
+//         let mut limit_tracker =
+//             LimitTracker::new(&mock_messeger, 100);
+//
+//         limit_tracker.set_value(80);
+//
+//         assert_eq!(mock_messeger.sent_messages.borrow().len(), 1);
+//     }
+// }
+//
+// //
+// // #[test]
+// // fn iterator_demonstration() {
+// //     let v1 = vec![1, 2, 3];
+// //     let mut v1_iter = v1.iter();
+// //
+// //     assert_eq!(v1_iter.next(), Some(&1));
+// //     assert_eq!(v1_iter.next(), Some(&2));
+// //     assert_eq!(v1_iter.next(), Some(&3));
+// //     assert_eq!(v1_iter.next(), None);
+// // }
+// //
+// // #[test]
+// // fn iterator_sum() {
+// //     let v1 = vec![1, 2, 3];
+// //     let v1_iter = v1.iter();
+// //     let total: i32 = v1_iter.sum();
+// //
+// //     assert_eq!(total, 6);
+// // }
+// //
+// // #[test]
+// // fn map_adapter() {
+// //     let v1 = vec![1, 2, 3];
+// //     let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+// //     assert_eq!(v2, vec![2, 3, 4]);
+// // }
+// //
+// // #[derive(PartialEq, Debug)]
+// // struct Shoe {
+// //     size: u32,
+// //     style: String,
+// // }
+// //
+// // fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+// //     shoes.into_iter()
+// //         .filter(|s| s.size == shoe_size)
+// //         .collect()
+// // }
+// //
+// // #[test]
+// // fn filters_by_size() {
+// //     let shoes = vec![
+// //         Shoe { size: 10, style: "sneaker".to_string() },
+// //         Shoe { size: 13, style: "sandal".to_string() },
+// //         Shoe { size: 10, style: "boot".to_string() },
+// //     ];
+// //
+// //     let in_my_size = shoes_in_my_size(shoes, 10);
+// //     assert_eq!(
+// //         in_my_size,
+// //         vec![
+// //             Shoe { size: 10, style: String::from("sneaker") },
+// //             Shoe { size: 10, style: String::from("boot") },
+// //         ]
+// //     )
+// // }
+// //
+// // struct Counter {
+// //     count: u32
+// // }
+// //
+// // impl Counter {
+// //     fn new() -> Counter {
+// //         Counter { count: 0 }
+// //     }
+// // }
+// //
+// // impl Iterator for Counter {
+// //     type Item = u32;
+// //
+// //     fn next(&mut self) -> Option<Self::Item> {
+// //         self.count += 1;
+// //
+// //         if self.count < 6 {
+// //             Some(self.count)
+// //         } else {
+// //             None
+// //         }
+// //     }
+// // }
+// //
+// // #[test]
+// // fn calling_next_directly() {
+// //     let mut counter = Counter::new();
+// //     assert_eq!(counter.next(), Some(1));
+// //     assert_eq!(counter.next(), Some(2));
+// //     assert_eq!(counter.next(), Some(3));
+// //     assert_eq!(counter.next(), Some(4));
+// //     assert_eq!(counter.next(), Some(5));
+// //     assert_eq!(counter.next(), None);
+// //     assert_eq!(counter.next(), None);
+// //
+// // }
+// //
+// // #[test]
+// // fn using_other_iterator_trait_methods() {
+// //     let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+// //         .map(|(a, b)| a * b)
+// //         .filter(|x| x % 3==0)
+// //         .sum();
+// //
+// //     assert_eq!(18, sum);
+// //
+// // }
+// //
---
+++ src/main.rs
@@ -0,0 +1,539 @@
+fn main() {
+
+    // let string_2: str = "ace";
+
+    println!("{}", string_2);
+    // let is_bigger = 1> 4;
+    // println!("Is 1 > 4 {}",is_bigger);
+   /* println!("1 +2 ={} and 8-5 = {}, and 15*3={}", 1u32 + 2, 8i32 - 5, 15 * 3);
+    println!("9/2={} but 9.0/2.0={}", 9u32 / 2, 9.0 / 2.0);*/
+
+   /* let number_64=4.0;
+    let number_32: f32 = 5.0;*/
+
+   /* let number:u32=14;
+    println!("{}", number);*/
+
+   /* let shadow_num=5;
+    let shadow_num = shadow_num + 5;
+    let shadow_num=shadow_num*2;
+    println!("{}", shadow_num);*/
+
+    /*let mut a_number=10;
+    println!("{}", a_number);
+    a_number = 22;
+
+    println!("{}", a_number);*/
+    // let a_number;
+    // let a_word = "Ten";
+    // println!("The number is {}", a_number);
+    // a_number=10;
+    // println!("The word is {}", a_word);
+}
+// fn main() {
+//     let a =1;
+//     let b = a;
+//     println!("{} -> {:p}", b,&b);
+//
+//     println!("{} -> {:p}", a, &a);
+// }
+// fn main() {
+//     let mut data = vec![1, 2, 3, 4];
+//     let data1 = vec![&data[0]];
+//     println!("data[0]:{:p}", &data[0]);
+//     for i in 0..100 {
+//         data.push(i);
+//     }
+//
+//     println!("data[0]: {:p}", &data[0]);
+//     println!("boxed:{:p}", &data1);
+// }
+// fn main() {
+//     let mut data = vec![1, 2, 3];
+//
+//     for item in data.iter_mut() {
+//         data.push(*item + 1);
+//     }
+// }
+
+// fn main() {
+//     let mut data:Vec<&u32> = Vec::new();
+//     push_local_ref(&mut data);
+//     println!("data:{:?}", data);
+// }
+//
+// fn push_local_ref(data: &mut Vec<&u32>) {
+//     let v = 42;
+//     data.push(&v);
+// }
+// fn main() {
+//     let mut data:Vec<&u32> = Vec::new();
+//     let v = 42;
+//     data.push(&v);
+//     println!("data: {:?}", data);
+// }
+// fn main() {
+//     let r = local_ref();
+//     println!("r: {:p}", r);
+//
+// }
+// fn local_ref<'a>() -> &'a i32 {
+//     let a = 40;
+//     &a
+// }
+
+// fn main() {
+//     let data = vec![1, 2, 3, 4];
+//     let data1 = &data;
+//     println!(
+//         "addr of value: {:p} ( {:p} ), \naddr of data {:p}, data1: {:p}",
+//         &data, data1, &&data, &data1
+//     );
+//     println!("sum of data1:{}", sum(data1));
+//
+//     // 堆上数据的地址是什么？
+//     println!(
+//         "addr of items :[{:p}, {:p}, {:p}, {:p}]",
+//         &data[0], &data[1], &data[2], &data[3]
+//     );
+//
+//     println!(
+//         "addr of items :[{}, {}, {}, {}]",
+//         &data[0], &data[1], &data[2], &data[3]
+//     );
+//
+//     println!(
+//         "addr of items :[{}, {}, {}, {}]",
+//         data[0], data[1], data[2], data[3]
+//     );
+// }
+//
+// fn sum(data: &Vec<u32>) -> u32 {
+//     println!("> addr of value: {:p}, addr of ref: {:p}", data, &data);
+//     data.iter().fold(0, |acc, x| acc + x)
+// }
+
+// fn main() {
+//     // let a1=11;
+//     // let b1 = a1;
+//     // println!("{}", a1);
+//     let data = vec![1, 2, 3, 4];
+//     let data1 = &data;
+//     println!("sum of data1 {}", sum(data1));
+//     // println!("data1: {:?}", data1);
+//     println!("sum of data: {}", sum(data));
+// }
+
+// fn sum(data: Vec<u32>) -> u32 {
+//     data.iter().fold(0, |acc,x| acc + x)
+// }
+// struct Point{
+// x:i32,
+// y:i32,
+// }
+// fn main() {
+//     let a = Box::new(Point { x: 1, y: 2 });
+//
+// }
+// use std::rc::Rc;
+// use std::cell::RefCell;
+// use crate::List::Nil;
+//
+//
+// #[derive(Debug)]
+// enum List{
+//     Cons(Rc<RefCell<i32>>,Rc<List>),
+//     Nil,
+// }
+//
+// fn main() {
+//     let value = Rc::new(RefCell::new(5));
+//     let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+//     let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+//     let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+//
+//     *value.borrow()+=10;
+//
+// }
+//
+// fn Cons(p0: Rc<RefCell<i32>>, p1: Rc<List>) -> T {
+//     todo!()
+// }
+//
+// // use std::fs::File;
+// // use std::io::{Error, ErrorKind, Read};
+// // use std::io;
+// // include!("myfile.rs");
+// //
+// // // // // // use std::thread;
+// // // // // // use std::time::Duration;
+// // // // // //
+// // // // // // // use std::cell::RefCell;
+// // // // // // // use std::rc::Rc;
+// // // // // // // use crate::List::{Cons, Nil};
+// // // // // // //
+// // // // // // // #[derive(Debug)]
+// // // // // // // enum List{
+// // // // // // //     Cons(Rc<RefCell<i32>>, Rc<List>),
+// // // // // // //     Nil,
+// // // // // // // }
+// // // // // // //
+// // // // // // // fn main() {
+// // // // // // //     let value = Rc::new(RefCell::new(5));
+// // // // // // //     let a = Rc::new(Cons(Rc::clone(&value),Rc::new(Nil)));
+// // // // // // //     let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+// // // // // // //     let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+// // // // // // //
+// // // // // // //     *value.borrow_mut() += 10;
+// // // // // // //     println!("a after = {:?}", a);
+// // // // // // //     println!("b after = {:?}", b);
+// // // // // // //     println!("c after = {:?}", c);
+// // // // // // // }
+// // // // // // // // use std::thread;
+// // // // // // // // use std::time::Duration;
+// // // // // // // //
+// // // // // // // // pub trait Iterator {
+// // // // // // // //     type Item;
+// // // // // // // //     fn next(&mut self) -> Option<Self::Item>;
+// // // // // // // // }
+// // // // // // // //
+// // // // // // // // enum List {
+// // // // // // // //     Cons(i32, Rc<List>),
+// // // // // // // //     Nil,
+// // // // // // // // }
+// // // // // // // //
+// // // // // // // // use crate::List::{Cons, Nil};
+// // // // // // // // use std::rc::Rc;
+// // // // // // // //
+// // // // // // // //
+// // // // // // // // fn main() {
+// // // // // // // //     let x  =5;
+// // // // // // // //     let y = &mut x;
+// // // // // // // //     // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+// // // // // // // //     // println!("count after creating a = {}", Rc::strong_count(&a));
+// // // // // // // //     //
+// // // // // // // //     // let b = Cons(3, Rc::clone(&a));
+// // // // // // // //     // println!("count after creating b = {}", Rc::strong_count(&a));
+// // // // // // // //     // {
+// // // // // // // //     //     let c = Cons(4, Rc::clone(&a));
+// // // // // // // //     //     println!("count after creating c = {}", Rc::strong_count(&a));
+// // // // // // // //     //
+// // // // // // // //     // }
+// // // // // // // //     // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// // // // // // // //
+// // // // // // // //
+// // // // // // // //
+// // // // // // // //     // let a = Rc::new(
+// // // // // // // //     //     Cons(5,
+// // // // // // // //     //          Rc::new(
+// // // // // // // //     //              Cons(10,
+// // // // // // // //     //                   Rc::new(Nil)))));
+// // // // // // // //     // let b = Cons(3, Rc::clone(&a));
+// // // // // // // //     // let c = Cons(4, Rc::clone(&a));
+// // // // // // // //     // let v1 = vec![1, 2, 3];
+// // // // // // // //     // let v1_iter = v1.iter();
+// // // // // // // //     // for x in v1_iter {
+// // // // // // // //     //     println!("{}", x);
+// // // // // // // //     // }
+// // // // // // // //     // let example_closure = |x| x;
+// // // // // // // //     // let demo2 = example_closure(2);
+// // // // // // // //     // let s = example_closure("asdfa".to_string());
+// // // // // // // //     //
+// // // // // // // //     // println!("{}", demo2);
+// // // // // // // //     // println!("{}", s);
+// // // // // // // //     // let simulated_user_specified_value = 10;
+// // // // // // // //     // let simulated_random_number = 7;
+// // // // // // // //     //
+// // // // // // // //     // generate_workout(
+// // // // // // // //     //     simulated_user_specified_value,
+// // // // // // // //     //     simulated_random_number
+// // // // // // // //     // );
+// // // // // // // // }
+// // // // // // // //
+// // // // // // // // struct Cacher<T>
+// // // // // // // //     where T: Fn(u32) -> u32
+// // // // // // // // {
+// // // // // // // //     calculation: T,
+// // // // // // // //     value: Option<u32>,
+// // // // // // // // }
+// // // // // // // //
+// // // // // // // // impl<T> Cacher<T>
+// // // // // // // //     where T: Fn(u32) -> u32
+// // // // // // // // {
+// // // // // // // //     fn new(calculation: T) -> Cacher<T> {
+// // // // // // // //         Cacher {
+// // // // // // // //             calculation,
+// // // // // // // //             value: None,
+// // // // // // // //         }
+// // // // // // // //     }
+// // // // // // // //
+// // // // // // // //     fn value(&mut self, arg: u32) -> u32 {
+// // // // // // // //         match self.value {
+// // // // // // // //             None => {
+// // // // // // // //                 let v = (self.calculation)(arg);
+// // // // // // // //                 self.value = Some(v);
+// // // // // // // //                 v
+// // // // // // // //             }
+// // // // // // // //             Some(v) => { v }
+// // // // // // // //         }
+// // // // // // // //     }
+// // // // // // // // }
+// // // // // // // //
+// // // // // // // //
+// // // // // // // // fn generate_workout(intensity: u32, random_number: u32) {
+// // // // // // // //     // let expensive_result = simulated_expensive_calculation(intensity);
+// // // // // // // //
+// // // // // // // //     let expensive_closure = |num| {
+// // // // // // // //         println!("calculatig slowly...");
+// // // // // // // //         thread::sleep(Duration::from_secs(2));
+// // // // // // // //         num
+// // // // // // // //     };
+// // // // // // // //
+// // // // // // // //     if intensity < 25 {
+// // // // // // // //         println!(
+// // // // // // // //             "Today, do {} pushups!",
+// // // // // // // //             expensive_closure(intensity)
+// // // // // // // //         );
+// // // // // // // //         println!(
+// // // // // // // //             "Next, do {} situps!",
+// // // // // // // //             expensive_closure(intensity)
+// // // // // // // //         );
+// // // // // // // //     } else {
+// // // // // // // //         if random_number == 3 {
+// // // // // // // //             println!("Take a break today! Remember to stay hydrated!");
+// // // // // // // //         } else {
+// // // // // // // //             println!(
+// // // // // // // //                 "Today, run for {} minutes!",
+// // // // // // // //                 expensive_closure(intensity)
+// // // // // // // //             );
+// // // // // // // //         }
+// // // // // // // //     }
+// // // // // // // // }
+// // // // // // // //
+// // // // // // // // fn simulated_expensive_calculation(intensity: u32) -> u32 {
+// // // // // // // //     println!("calculating slowly...");
+// // // // // // // //     thread::sleep(Duration::from_secs(2));
+// // // // // // // //     intensity
+// // // // // // // // }
+// // // // // // // //
+// // // // // // fn main() {
+// // // // // //    let handle= thread::spawn(||{
+// // // // // //         for i in 1..10{
+// // // // // //             println!("{}", i);
+// // // // // //             thread::sleep(Duration::from_millis(1));
+// // // // // //         }
+// // // // // //     });
+// // // // // //
+// // // // // //     handle.join();//.unwrap();
+// // // // // //     for i in 1..5{
+// // // // // //         println!(">>> {}", i);
+// // // // // //         thread::sleep(Duration::from_millis(1));
+// // // // // //     }
+// // // // // //
+// // // // // // }
+// // // // // use std::thread;
+// // // // // use std::sync::Mutex;
+// // // // //
+// // // // // fn main() {
+// // // // //     let m = Mutex::new(5);
+// // // // //     {
+// // // // //         let mut num = m.lock().unwrap();
+// // // // //         *num = 6;
+// // // // //     }
+// // // // //     println!("{:?}", m)
+// // // // //
+// // // // //     // let v = vec![1, 2, 3];
+// // // // //     //
+// // // // //     // let handle = thread::spawn(move || {
+// // // // //     //     println!("{:?}", v);
+// // // // //     // });
+// // // // //     // // drop(v);
+// // // // //     // handle.join().unwrap();
+// // // // // }
+// // // // use std::sync::{Mutex, Arc};
+// // // // use std::thread;
+// // // //
+// // // // fn main() {
+// // // //     let counter = Arc::new(Mutex::new(0));
+// // // //
+// // // //     let mut handles = vec![];
+// // // //
+// // // //     for _ in 0..10 {
+// // // //         let counter = Arc::clone(&counter);
+// // // //         let handle = thread::spawn(move || {
+// // // //             let mut num = counter.lock().unwrap();
+// // // //
+// // // //             *num += 1;
+// // // //         });
+// // // //
+// // // //         handles.push(handle);
+// // // //     }
+// // // //
+// // // //     for handle in handles {
+// // // //         handle.join().unwrap();
+// // // //     }
+// // // //
+// // // //     println!("Result: {}", *counter.lock().unwrap());
+// // // // }
+// // //
+// // // pub struct AveragedCollection {
+// // //     list: Vec<i32>,
+// // //     average: f64,
+// // // }
+// // //
+// // // impl AveragedCollection {
+// // //     pub fn add(&mut self, value: i32) {
+// // //         self.list.push(value);
+// // //         self.update_average();
+// // //     }
+// // //
+// // //     pub fn remove(&mut self) -> Option<i32> {
+// // //         let result = self.list.pop();
+// // //         match result {
+// // //             None => { None }
+// // //             Some(v) => {
+// // //                 self.update_average();
+// // //                 v
+// // //             }
+// // //         }
+// // //     }
+// // //
+// // //     pub fn average(&self) -> f64 {
+// // //         self.average;
+// // //     }
+// // //
+// // //
+// // //     fn update_average(&mut self) {
+// // //         let total :i32 = self.list.iter().sum();
+// // //         self.average = total as f64 / self.list.len() as f64;
+// // //     }
+// // // }
+// // //
+// // // fn main() {}
+// // // use hello_world::{Draw, Screen, Button};
+// // //
+// // // struct SelectBox {
+// // //     width: u32,
+// // //     height: u32,
+// // //     options: Vec<String>,
+// // // }
+// // //
+// // // impl Draw for SelectBox {
+// // //     fn draw(&self) {
+// // //         println!("Draw SelectBox")
+// // //     }
+// // // }
+// // //
+// // // fn main() {
+// // //     // let screen = Screen{ components: vec![
+// // //     //     Box::new(SelectBox{
+// // //     //         width: 75,
+// // //     //         height: 10,
+// // //     //         options: vec![
+// // //     //             String::from("Yes"),
+// // //     //             String::from("Maybe"),
+// // //     //             String::from("No"),
+// // //     //         ],
+// // //     //     }),
+// // //     //     Box::new(Button{
+// // //     //         width: 50,
+// // //     //         height: 10,
+// // //     //         label: "Ok".to_string(),
+// // //     //     }),
+// // //     // ] };
+// // //
+// // //     let screen = Screen {
+// // //         components: vec![
+// // //             Box::new(String::from("Hi"))
+// // //         ]
+// // //     };
+// // //
+// // //     screen.run();
+// // // }
+// //
+// // fn main() {
+// //    //  let f = File::open("hello.txt");
+// //    // let f =  match f {
+// //    //      Ok(file) => {file}
+// //    //      Err(err) => match err.kind(){
+// //    //          ErrorKind::NotFound => match File::create("hello.txt"){
+// //    //              Ok(fc) => {fc}
+// //    //              Err(e) => panic!("Error createing file: {:?}",e)
+// //    //          }
+// //    //          other_err => {panic!("Error open the file {:?}",other_err)}
+// //    //      }
+// //    //  };
+// //
+// //     // let f = File::open("hello.txt").unwrap_or_else(|error|{
+// //     //     if error.kind()==ErrorKind::NotFound {
+// //     //         File::create("hello.txt").unwrap_or_else(|error|{
+// //     //             panic!("Error createing file: {:?}",error);
+// //     //         })
+// //     //     }else {
+// //     //         panic!("Error open the file {:?}",other_err)
+// //     //     }
+// //     // });
+// //
+// //     //////
+// // let result = read_username_from_file();
+// //
+// //     // // println!("{}","00008".parse::<i32>().unwrap());
+// //     // let num = 12399400;
+// //     // let r = Solution::reverse(num);
+// //     //
+// //     // // let aaa =num.to_string().chars().skip(1)
+// //     // println!("{}", r);
+// // }
+// //
+// // fn read_username_from_file() -> Result<String,io::Error>{
+// //     // let f = File::open("hello.txt");
+// //     // let mut f =match f {
+// //     //     Ok(file) => {file}
+// //     //     Err(e) => { return Err(e); }
+// //     // };
+// //     //
+// //     // let mut s =String::new();
+// //     // match f.read_to_string(&mut s) {
+// //     //     Ok(_) => {Ok(s)}
+// //     //     Err(e) => {e}
+// //     // }
+// //     // ////////
+// //     // let mut f = File::open("hello.txt")?;
+// //     // let mut s = String::new();
+// //     // f.read_to_string(&mut s)?;
+// //     // Ok(s)
+// //
+// //     ////////
+// //     let mut s = String::new();
+// //     let mut f = File::open("hello.txt")?.read_to_string(&mut s)?;
+// //     Ok(s)
+// // }
+// //
+// // struct Solution;
+// //
+// // impl Solution {
+// //     pub fn reverse(x: i32) -> i32 {
+// //         // let x1 = x.to_string();
+// //         // for a in x.chars().rev() {
+// //         //     println!("{}", a);
+// //         // }
+// //         let mut x2 = x;
+// //         let mut x3 = "".to_string();
+// //         let pre_symbol = if x < 0 {
+// //             x2 *= -1;
+// //             for c in x.to_string().chars().skip(1) {
+// //                 println!("{}", c);
+// //                 x3.push(c);
+// //             }
+// //             -1
+// //         } else {
+// //              x3.push_str(x.to_string().rev().as_str());
+// //             1
+// //         };
+// //
+// //         let mut x3 = x3.parse::<i32>().unwrap();
+// //         x3 *= pre_symbol;
+// //
+// //         x3
+// //     }
+// // }
